4986208d6a90eadd2eded027839921fe64980d29
Gabriel Menezes Alves <gabriel_cm15@hotmail.com>
2024-08-27 21:46:38 -0300

    pesquisando item

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,17 @@ app.get("/listar-tarefas", async function(req, res){
     res.json(dados);
 });
 
+app.get("/ler-tarefa", async function(req, res){
+    const mongo = await conexao.connect();
+    const col = mongo.db("gabriel").collection("tarefas");
+
+    const id = new ObjectId('66ce66f68c73b9e520d51989');
+    const tarefa = await col.findOne({_id: id});
+
+    res.json(tarefa);
+
+});
+
 app.listen(3003, function(){
     console.log("servidor iniciado");
 });
